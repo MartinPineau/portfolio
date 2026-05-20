82c6e18a73adcba4769c723db690045320707cb8
Martin <martinpineau23@gmail.com>
2026-05-20 16:49:24 +0200

    footer update color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,9 @@ const Footer = () => {
     <div>
       <footer className="bg-[var(--color-bg-dark)] text-center py-6">
         <p className="text-sm text-[var(--color-gray-medium)]">
-          <FaInstagram className="inline-block mx-2 w-8 h-8 text-black" />
-          <FaLinkedin className="inline-block mx-2 w-8 h-8 text-black" />
-          <FaEnvelope className="inline-block mx-2 w-8 h-8 text-black" />
+          <FaInstagram className="inline-block mx-2 w-8 h-8 text-[var(--color-main-dark)]" />
+          <FaLinkedin className="inline-block mx-2 w-8 h-8 text-[var(--color-main-dark)]" />
+          <FaEnvelope className="inline-block mx-2 w-8 h-8 text-[var(--color-main-dark)]" />
         </p>
         <p className="text-sm text-[var(--color-gray-medium)] mt-4">
             Madelyn Torff 2021
