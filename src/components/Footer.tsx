dfceed146c7114b29974bfce86be97cb4c209cdf
Martin <martinpineau23@gmail.com>
2026-05-20 16:42:08 +0200

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,17 @@
+import { FaEnvelope, FaInstagram, FaLinkedin } from "react-icons/fa";
+
 const Footer = () => {
   return (
-    <></>
+    <div>
+      <footer className="bg-[var(--color-bg-dark)] text-center py-6">
+        <p className="text-sm text-[var(--color-gray-medium)]">
+          <FaInstagram className="inline-block mx-2 w-8 h-8" />
+          <FaLinkedin className="inline-block mx-2 w-8 h-8" />
+          <FaEnvelope className="inline-block mx-2 w-8 h-8" />
+        </p>
+      </footer>
+    </div>
   );
-};  
+};
 
 export default Footer;
